68410e727fb21be295d7783153d5807079948e26
richard-office <richard@times10.co>
2019-09-03 18:31:10 +0800

    - remove config.js

--- config/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyAmbZnWZiDduaH58dnPAbdB9Sj0jVY9mYo",
-  authDomain: "simplest-chat-46766.firebaseapp.com",
-  databaseURL: "https://simplest-chat-46766.firebaseio.com",
-  projectId: "simplest-chat-46766",
-  storageBucket: "simplest-chat-46766.appspot.com",
-  messagingSenderId: "992995873865",
-  appId: "1:992995873865:web:b4216a9765d2fec5"
-}
